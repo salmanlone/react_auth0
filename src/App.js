ac8ac88ca6baac0c7bc3ac87418e583f6c2d6533
Lone <salman.lone@monster.com>
2019-11-05 17:35:31 +0800

    Routing is up and running

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+
 function App() {
-  return <div className="App" />;
+  return (
+    <>
+      <Route path="/" exact component={Home} />
+      <Route path="/profile" component={Profile} />
+    </>
+  );
 }
 
 export default App;
