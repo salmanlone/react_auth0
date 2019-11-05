3c527f5608849a20f08700f679d5084c17747ed2
Lone <salman.lone@monster.com>
2019-11-05 17:41:49 +0800

    App skeleton is complete but CSS is missing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+import Nav from "./components/nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <Route path="/" exact component={Home} />
       <Route path="/profile" component={Profile} />
     </>
